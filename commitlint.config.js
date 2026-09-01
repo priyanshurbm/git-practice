08703176602755c6690a8d057df67bd6e39ab4c1
Rbmsoft <rbmsoft@Rbmsofts-MacBook-Air-3.local>
2026-09-01 11:20:49 +0530

    TICKET-102: Fix commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,25 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [2, 'always', 72],
-    'type-enum': [0],
-    'header-pattern': [2, 'always', /^TICKET-\d+:\s.+$/],
-  },
+  extends: ['@commitlint/config-conventional'],
+
+  plugins: [
+    {
+      rules: {
+        'ticket-format': (parsed) => {
+          const pattern = /^TICKET-\d+:\s.+$/;
+          return [
+            pattern.test(parsed.header),
+            'Commit must match TICKET-123: Description',
+          ];
+        },
+      },
+    },
+  ],
+
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'type-enum': [0],
+    'type-empty': [0],
+    'subject-empty': [0],
+    'ticket-format': [2, 'always'],
+  },
 };
