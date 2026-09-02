a803c36fa2b3df409cd1d2570e5de5ca27c22680
Rbmsoft <rbmsoft@Rbmsofts-MacBook-Air-3.local>
2026-09-02 10:42:46 +0530

    TICKET-103: Format commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(TICKET-\d+): (.+)$/,
-      headerCorrespondence: ['type', 'subject'],
-    },
-  },
-  rules: {
-    'header-max-length': [2, 'always', 72],
-    'type-empty': [2, 'never'],
-    'subject-empty': [2, 'never'],
-  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(TICKET-\d+): (.+)$/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+  },
 };
